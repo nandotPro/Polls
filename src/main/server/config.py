f476cadf459e0977ef1216f2d2b6143b2b52d5ed
NaN <ipfilus@gmail.com>
2025-01-27 21:57:26 -0300

    feat: add errors to server

--- src/main/server/config.py
+++ src/main/server/config.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from src.models.mongodb.connection.mongo_connection import mongo_connection_handler
 from src.models.redis.connection.redis_connection import redis_connection_handler
+from src.errors.error_handler import handle_errors
+from src.errors.error_types.http_error import HttpError
 
 def create_app():
     app = Flask(__name__)
@@ -21,5 +23,9 @@ def create_app():
     # Registrar blueprints
     app.register_blueprint(auth_routes_bp, url_prefix='/auth')
     app.register_blueprint(poll_routes_bp, url_prefix='/polls')
+    
+    # Registrar handler de erros
+    app.register_error_handler(HttpError, handle_errors)
+    app.register_error_handler(Exception, handle_errors)
     
     return app 
