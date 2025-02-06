8a9c6b536bd4ef381d69a0f91cf1660988256596
NaN <ipfilus@gmail.com>
2025-02-05 22:10:15 -0300

    fix: removing close erros

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
+import signal
+import sys
 from src.main.server.config import create_app
+
+def signal_handler(sig, frame):
+    print('\nEncerrando a aplicação graciosamente...')
+    sys.exit(0)
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=3000,
-        debug=True
-    )
+    signal.signal(signal.SIGINT, signal_handler)
+    try:
+        app.run(
+            host="0.0.0.0",
+            port=3000,
+            debug=True
+        )
+    except KeyboardInterrupt:
+        print('\nEncerrando a aplicação graciosamente...')
+        sys.exit(0)
